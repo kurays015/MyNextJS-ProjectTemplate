a08fe9d10c7b1483682bb61c2a3b8f2f211c8b9f
Christzxz <christnarvarte15@gmail.com>
2024-04-15 21:46:19 +0800

    add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import SigninForm from "@/components/SigninForm";
+import { PrismaClient } from "@prisma/client";
 
 export default async function Home() {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
   return (
     <main className="max-w-7xl mx-auto">
       <h1 className="text-2xl text-center">
         My NextJS | TypeScript | Next-Auth | Shadcn-ui Template (with darkmode)!
       </h1>
+      <div>Users from DB: {users.map(user => user.email).join(", ")}</div>
     </main>
   );
 }
