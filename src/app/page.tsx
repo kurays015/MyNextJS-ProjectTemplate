29bb60f3da55025cf8b9d8852467421ef9c6eb09
Christzxz <christnarvarte15@gmail.com>
2024-04-15 21:54:04 +0800

    fix users render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export default async function Home() {
   const prisma = new PrismaClient();
   const users = await prisma.user.findMany();
+  console.log(users);
   return (
     <main className="max-w-7xl mx-auto">
       <h1 className="text-2xl text-center">
@@ -10,9 +11,9 @@ export default async function Home() {
       </h1>
       <div>
         Users from DB:{" "}
-        {users
-          .map((user, index) => <h5 key={index}>{user.email}</h5>)
-          .join(", ")}
+        {users.map((user, index) => (
+          <h5 key={index}>{user.email}</h5>
+        ))}
       </div>
     </main>
   );
